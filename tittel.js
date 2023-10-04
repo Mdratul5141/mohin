0c2c0ab4943df7226923a76f6b7b419b5460be5e
mafuzur rahamen <mdratulhasun9084@gamil.com>
2023-10-04 10:56:39 +0600

    js

---
+++ tittel.js
@@ -0,0 +1,54 @@
+document.getElementById("rakib").innerText="ohow ";
+let x,y,z;
+x=20;
+y=40;
+z=x+y;
+document.write(z) 
+//variable
+let mondol ="anik" ;
+document.write(mondol)
+// function
+function moina(amin,amin2){
+document.write( '<br>' +amin +amin2);
+}
+// function call
+moina("ARIF VAi",5);
+//object
+let rakib ={
+    Name : "mahfuz",
+    age:21,
+    gender:"male",
+    color:"red",
+};
+document.write(rakib.gender ,'<br>',rakib.color)
+//Array
+let mahfuz = [
+    "Saab",
+  "Volvo",
+  "BMW",
+  "habib",
+  "nabib",
+"soiti",
+];
+document.write(mahfuz[3]);
+// function call
+moina("a",5);
+moina("b",6);
+moina("c",5);
+
+let car =`
+
+<h1>I am A Mahfuz</h1>
+`
+document.write(car)
+
+// ()=first;
+// {}=second;
+// []=third
+
+
+
+
+
+
+
